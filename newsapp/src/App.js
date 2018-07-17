cd56f2f4fb52132157798f090382593a449b81f5
Jose Viteri <Jose@Joses-MacBook-Pro.local>
2018-07-17 17:38:31 -0400

    changes on app.js

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -14,8 +14,8 @@ const App = () =>
       <Wrapper>
         <Route exact path="/" component={About} />
         <Route exact path="/about" component={About} />
-        <Route exact path="/discover" component={Discover} />
-        <Route exact path="/search" component={Search} />
+        {/* <Route exact path="/discover" component={Discover} />
+        <Route exact path="/search" component={Search} /> */}
       </Wrapper>
       <Footer />
     </div>
